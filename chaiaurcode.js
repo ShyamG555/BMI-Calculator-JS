7923a93900f6df7f026d3e7bf7c0f539840dfbc5
ShyamG555 <125969499+ShyamG555@users.noreply.github.com>
2024-07-01 21:03:56 +0545

    added main files
    added main files

---
+++ chaiaurcode.js
@@ -0,0 +1,91 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if(height === '' || height <0 || isNaN(height)){
+        results.innerHTML = `Please give a valid height ${height}`
+    }else if (weight === '' || weight < 0 || isNaN(weight)) {
+        
+        results.innerHTML = `Please give a valid weight ${height}`
+    }else{
+        const bmi = (weight / ((height * height)/10000)).toFixed(2);
+        results.innerHTML = `<span> ${bmi}</span>`
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector('form')
+
+// // this usecase will give you empty
+// // const height = parseInt (document.querySelector('#height').value)
+
+// form.addEventListener('submit', function(e){
+//     e.preventDefault()
+
+//    const height = parseInt (document.querySelector('#height').value)
+//    const weight = parseInt (document.querySelector('#weight').value)
+//    const results =  document.querySelector('#results')
+
+//    if(height === '' || height < 0 || isNaN(height)){
+//     results.innerHTML = `Please enter a valid height ${height}`
+//    }else if(weight === '' || height < 0 || isNaN(weight)){
+//     results.innerHTML = `Please enter a valid Weight ${weight}`
+//    } else {
+//     const bmi = (weight /((height*height)/10000)).toFixed(2)
+
+//     // show the result
+
+//     results.innerHTML = `<span>${bmi}</span>`
+
+//     if(bmi < 18.6){
+//         results.innerHTML += `<p>Your bmi ${bmi} comes Underweight</p>`
+//     }else if(bmi > 18.6 && bmi < 24.9) {
+//         results.innerHTML += `<p>Your bmi ${bmi} comes Normal</p>`
+//     }else{
+//         results.innerHTML += `<p>Your bmi ${bmi} comes Overweight</p>`
+//     }
+//    }
+
+
+// })
